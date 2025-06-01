26f4d8706db4ad2a5eb356214d435456f0ae7de5
Yogeshwar B. <byogeshwarreddy@gmail.com>
2025-06-01 18:58:25 +0530

    Implemented DFS

--- src/GraphSearch.tsx
+++ src/GraphSearch.tsx
@@ -13,7 +13,7 @@ const GraphSearch = () => {
   ] = useState<SearchParams>({
     source: 44,
     destination: 8,
-    search: "bfs",
+    search: "dfs",
   })
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -34,7 +34,15 @@ const GraphSearch = () => {
       destination >= 0 &&
       destination < 100
     ) {
-      bfs(source, destination)
+      // Clear previous grid colors
+      for (let i = 0; i < 100; i++) {
+        paintN(i, "")
+      }
+      if (search === "bfs") {
+        bfs(source, destination)
+      } else if (search === "dfs") {
+        dfs(source, destination)
+      }
       console.log(`${source} - ${destination}`)
     } else {
       console.error("Invalid source or destination")
@@ -153,7 +161,7 @@ async function bfs(source: number, destination: number) {
   ]
 
   const visit: number[] = [source]
-  const seen = new Set<number>([]) 
+  const seen = new Set<number>([])
   paintN(source, "blue")
 
   while (visit.length > 0) {
@@ -171,7 +179,7 @@ async function bfs(source: number, destination: number) {
         let newN = newI * 10 + newJ
         if (seen.has(newN)) continue
 
-        seen.add(newN) 
+        seen.add(newN)
         if (newN != source && newN != destination) {
           paintN(newN, "red")
         }
@@ -187,6 +195,54 @@ async function bfs(source: number, destination: number) {
   }
 }
 
+async function dfs(source: number, destination: number) {
+  const dir = [
+    { i: -1, j: 0 }, // Up
+    { i: 1, j: 0 }, // Down
+    { i: 0, j: -1 }, // Left
+    { i: 0, j: 1 }, // Right
+  ]
 
+  const visit: number[] = [source]
+  const seen = new Set<number>([])
+  paintN(source, "blue")
+
+  while (visit.length > 0) {
+    await new Promise((resolve) => setTimeout(resolve, 100))
+    const current = visit.shift()
+    if (current === undefined) continue
+    if (current === destination) {
+      console.log(`Found destination ${destination} from ${source}`)
+      paintN(current, "green")
+      return
+    }
+    // paintN(current, "black")
+    if (seen.has(current)) {
+      paintN(current, "violet")
+      continue
+    }
+
+    seen.add(current)
+    if (current != source) {
+      paintN(current, "red")
+    } else {
+      paintN(current, "blue")
+    }
+
+    let curI = Math.floor(current / 10)
+    let curJ = current % 10
+    for (let d of dir) {
+      let newI = curI + d.i
+      let newJ = curJ + d.j
+      if (newI < 0 || newI >= 10 || newJ < 0 || newJ >= 10) continue
+
+      let newN = newI * 10 + newJ
+      if (newN !== source) {
+        visit.unshift(newN)
+        paintN(newN, "yellow")
+      }
+    }
+  }
+}
 
 export default GraphSearch
